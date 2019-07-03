7cbd4ca244aa317c6e5e193a10a02ceb135c1ef6
Thomas Anten <thomas.anten@sogeti.com>
2019-07-03 17:46:00 +0200

    Changed the css framework from bootstrap to Foundation

--- app/templates/default.tpl.php
+++ app/templates/default.tpl.php
@@ -1,13 +1,15 @@
 <?php if(!defined('INCLUDED')) exit('This file cannot be opened directly'); ?>
 
 <!DOCTYPE html>
-<html>
+<html class="no-js" lang="en" dir="ltr">
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?php echo $config['site_title']; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <?php echo $html->css('css/bootstrap.min.css'); ?>
-    <?php echo $html->css('css/bootstrap-theme.min.css'); ?>
+    <?php echo $html->css('css/foundation.css'); ?>
+    <?php echo $html->css('css/foundation.min.css'); ?>
     <?php echo $html->css('css/app.css'); ?>
   </head>
   <body>
@@ -16,7 +18,10 @@
     <?php echo template_content(); ?>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <?php echo $html->js('js/bootstrap.min.js'); ?>
+    <?php echo $html->js('js/vendor/jquery.js'); ?>
+    <?php echo $html->js('js/vendor/what-input.js'); ?>
+    <?php echo $html->js('js/vendor/foundation.js'); ?>
     <?php echo $html->js('js/app.js'); ?>
+
   </body>
 </html>
